eb2b9b8a3a457989ef562047f233a137b2f3f752
Pedro Souza <pedro@vencis.com.br>
2016-12-04 16:22:02 -0200

    Imporving graphs information

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -14,13 +14,13 @@ class WeatherList extends Component {
           <tr key={name}>
               <td>{name}</td>
               <td>
-                  <Chart data={temps} color="orange"/>
+                  <Chart data={temps} color="orange" units="K"/>
               </td>
               <td>
-                  <Chart data={pressures} color="gray"/>
+                  <Chart data={pressures} color="gray" units="hPa"/>
               </td>
               <td>
-                  <Chart data={humidities} color="purple"/>
+                  <Chart data={humidities} color="purple" units="%"/>
               </td>
           </tr>
         );
@@ -32,9 +32,9 @@ class WeatherList extends Component {
                 <thead>
                 <tr>
                     <th>City</th>
-                    <th>Temperature</th>
-                    <th>Pressure</th>
-                    <th>Humidity</th>
+                    <th>Temperature(K)</th>
+                    <th>Pressure(hPa)</th>
+                    <th>Humidity(%)</th>
                 </tr>
                 </thead>
 
